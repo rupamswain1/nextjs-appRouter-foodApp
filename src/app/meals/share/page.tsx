1cf37af432ad3e40558dd4f9a6383694b8996f39
rupamswain1 <rupamswain1@gmail.com>
2024-10-27 09:50:22 +0530

    share page styling

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -2,34 +2,34 @@ import Button from '@/components/Button/Button';
 import styles from './page.module.css';
 
 const SharePage = () => {
-  return <div>
+  return <div className={styles.shareBody}>
     <header className={styles.headerContainer}>
       <h1 className='heading'>Share your 
         <span className='highlightedText'> favourite meal</span>
       </h1>
-      <p>or any other meal you feel needs sharing</p>
+      <p className='subHeaderText'>or any other meal you feel needs sharing</p>
     </header>
-    <main className='styles.shareForm'>
+    <main className={styles.shareForm+' subHeaderText'}>
       <form>
-        <div>
-          <p>
+        <div className={styles.nameContainer}>
+          <p className={styles.labelAndInput}>
             <label htmlFor='name'>Your Name</label>
             <input type="text" id="name" name='name' required/>
           </p>
-          <p>
+          <p className={styles.labelAndInput}>
             <label htmlFor='email'>Your Email</label>
             <input type="email" id="email" name='email' required/>
           </p>
         </div>
-        <p>
+        <p className={styles.labelAndInput}>
           <label htmlFor='title'>Title</label>
           <input type="text" id="title" name="title" required/>
         </p>
-        <p>
+        <p className={styles.labelAndInput}>
           <label htmlFor='shortSummary'>Short Summmary</label>
           <input type="text" id="shortSummary" name="summary"/>
         </p>
-        <p>
+        <p className={styles.labelAndInput}>
           <label htmlFor='instructions'>
             Instructions
           </label>
